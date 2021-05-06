bb654acfb201478b552239a2765b78de1345ce03
Murray Ireland <murray@craftprospect.com>
2021-05-06 16:47:46 +0100

    Added debugging test code

---
+++ debugging-tests/test1.py
@@ -0,0 +1,18 @@
+"""
+Ref: https://towardsdatascience.com/a-comprehensive-guide-to-debugging-python-scripts-in-vs-code-b9f9f777d4b8
+"""
+
+
+import pandas as pd
+
+df = pd.DataFrame(data={"id": ['a', 'b', 'c'],
+                        "value": [1, 2, 3]})
+
+def multiply_value(df, multiplier):
+    df = df.copy()
+    df["value"] = df["value"] * multiplier
+
+multiplier_list = [1, 2, "3"]
+
+for mult in multiplier_list:
+    multiply_value(df, mult)
